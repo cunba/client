78c2d94cd7bc9d47a4063daa88da04b100e13e3f
cunba <ire.cunba@gmail.com>
2022-06-04 02:30:33 +0200

    añadir format en topics

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import configparser
 import logging
 import time
 from events import *
+from events import disband_event_disband_mac_sync
 from utils.topics import *
 from actions import *
 
@@ -18,7 +19,8 @@ def getConfig():
 
 def main():
 
-    connection = ConnectRaspberryTilebox()
+    disbeacMac = ''
+    disbandMac = connection = ConnectRaspberryTilebox()
 
     logging.basicConfig(level=logging.INFO)
     logging.info('Start of main.')
@@ -26,24 +28,38 @@ def main():
 
     # Creating the messaging for publications
     
-    DisbandActionAmbientNoise(config)
-    DisbandActionHeartRate(config)
-    DisbandActionHumidity(config)
-    DisbandActionLightning(config)
-    DisbandActionOxygen(config)
-    DisbandActionPressure(config)
-    DisbandActionTemperature(config)
-    DisbandActionPair(config)
-    DisbeacActionLocation(config)
-    DisbeacActionPair(config)
+    disband_action_ambient_noise = DisbandActionAmbientNoise(config)
+    disband_action_heart_rate = DisbandActionHeartRate(config)
+    disband_action_humidity = DisbandActionHumidity(config)
+    disband_action_lightning = DisbandActionLightning(config)
+    disband_action_oxygen = DisbandActionOxygen(config)
+    disband_action_pressure = DisbandActionPressure(config)
+    disband_action_temperature = DisbandActionTemperature(config)
+    disband_action_pair = DisbandActionPair(config)
+    disbeac_action_location = DisbeacActionLocation(config)
+    disbeac_action_pair = DisbeacActionPair(config)
 
     # Subscribing to the topics
 
-    DisbandEventDisbandMacSync(config, TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_SYNC)
-    DisbandEventDisbandMacSyncAlarm(config, TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_SYNC_ALARM)
-    DisbandEventDisbandMacSyncMeasureTimes(config, TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_SYNC_MEASURE_TIMES)
-    DisbandEventDisbandMacVibrate(config, TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_VIBRATE)
-    DisbeacEventDisbandMacActiveDisbandMac(config, TopicsSubscriptions.DISBEACS_EVENT_DISBEAC_MAC_ACTIVE_DISBAND_MAC)
+    disband_event_sync_topic = str(TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_SYNC)
+    disband_event_sync_topic.format(disbandMac = disbandMac)
+    DisbandEventDisbandMacSync(config, disband_event_sync_topic)
+
+    disband_event_sync_alarm_topic = str(TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_SYNC_ALARM)
+    disband_event_sync_alarm_topic.format(disbandMac = disbandMac)
+    DisbandEventDisbandMacSyncAlarm(config, disband_event_sync_alarm_topic)
+
+    disband_event_sync_measure_times_topic = str(TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_SYNC_MEASURE_TIMES)
+    disband_event_sync_measure_times_topic.format(disbandMac = disbandMac)
+    DisbandEventDisbandMacSyncMeasureTimes(config, disband_event_sync_measure_times_topic)
+
+    disband_event_vibrate_topic = str(TopicsSubscriptions.DISBANDS_EVENT_DISBAND_MAC_VIBRATE)
+    disband_event_vibrate_topic.format(disbandMac = disbandMac)
+    DisbandEventDisbandMacVibrate(config, disband_event_vibrate_topic)
+
+    disbeac_event_active_topic = str(TopicsSubscriptions.DISBEACS_EVENT_DISBEAC_MAC_ACTIVE_DISBAND_MAC)
+    disbeac_event_active_topic.format(disbeacMac = disbeacMac, disbandMac = disbandMac)
+    DisbeacEventDisbandMacActiveDisbandMac(config, disbeac_event_active_topic)
 
     while (True):
         time.sleep(1)
